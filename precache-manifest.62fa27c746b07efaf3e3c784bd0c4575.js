ca97d462f8a885a670fc5ee5ff2b67bfe9c70b62
KhalfaouiAnis <aniskhalfaoui9@gmail.com>
2019-10-07 00:37:21 +0100

    Updates

---
+++ precache-manifest.62fa27c746b07efaf3e3c784bd0c4575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee76cc1781d6948d47391ed7eb894ca",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "a0f718a87877592b89e9",
+    "url": "/contact-manager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "aea73e283a84a6ce9f15",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "a0f718a87877592b89e9",
+    "url": "/contact-manager/static/js/2.06add1ab.chunk.js"
+  },
+  {
+    "revision": "aea73e283a84a6ce9f15",
+    "url": "/contact-manager/static/js/main.b56f706e.chunk.js"
+  },
+  {
+    "revision": "bd83f98a5099e799e338",
+    "url": "/contact-manager/static/js/runtime-main.9183930f.js"
+  }
+]);
